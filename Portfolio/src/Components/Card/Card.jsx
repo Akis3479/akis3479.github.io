26cbec9fd1fd132dce9c1680703a4cf6520f98ce
Akis3479 <csd3479@csd.uoc.gr>
2024-04-22 20:04:01 +0300

    add download resume btn

--- Portfolio/src/Components/Card/Card.jsx
+++ Portfolio/src/Components/Card/Card.jsx
@@ -1,20 +1,23 @@
 import profile from './assets/image.jpg';
+import ResumeDoc from './assets/resume.pdf';
 import "./styles.css";
 
 
-export const Card = ({isOpen}) => {
+export const Card = ({ isOpen }) => {
     return (
         <div className={`card-container ${isOpen}`}>
             <div className="card">
-                    <img src={profile} loading="lazy" />
+                <img src={profile} loading="lazy" />
                 <div>
                     <h2>Akis Sougias</h2>
                     <h3>Frontend developer <br></br> Design Pattern Enthusiast</h3>
                     <p>
-                    As a dedicated and dependable Computer Science professional, I excel in web application development. 
-                    I bring strong organizational skills, a proactive approach, and a collaborative mindset to any team. 
+                        As a dedicated and dependable Computer Science professional, I excel in web application development.
+                        I bring strong organizational skills, a proactive approach, and a collaborative mindset to any team.
                     </p>
-                    <button>Resume</button>
+                    <a href={ResumeDoc} download="AkisSougias_Resume" target="_blank">
+                        <button>Resume</button>
+                    </a>
                 </div>
             </div>
         </div>
