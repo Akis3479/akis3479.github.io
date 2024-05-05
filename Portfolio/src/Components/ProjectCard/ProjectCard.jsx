bb6cded1850d5d8462e879e535bf5f5a07db810a
Akis3479 <csd3479@csd.uoc.gr>
2024-05-05 17:08:32 +0300

    git links in project cards

--- Portfolio/src/Components/ProjectCard/ProjectCard.jsx
+++ Portfolio/src/Components/ProjectCard/ProjectCard.jsx
@@ -6,6 +6,7 @@ const cards = [
         id: "green",
         description: "Technology : MERN stack React, MongoDB, ExpressJS, Node",
         footer: "Bachelor's Thesis project",
+        link: "https://github.com/Akis3479/ThesisProject-InternshipApp",
         more: "Designed and developed full-stack web application platform to facilitate university students' access to internship opportunities. Implemented with React Forms for seamless application submission and file uploads. Admin capabilities include application management via intuitive tables. Integrated authentication via the university's email database for secure access.",
     },
     {
@@ -13,6 +14,7 @@ const cards = [
         id: "blue",
         description: "Technology : Java",
         footer: "Two-Person University Project",
+        link: "https://github.com/Akis3479/Information-Retrieval-Systems-Project---hy463",
         more: "Engineered an Information Retrieval System encompassing indexing, question rating, and results evaluation functionalities.",
     },
     {
@@ -20,6 +22,7 @@ const cards = [
         id: "red",
         description: "Technology : C++",
         footer: "Three-Person University Project",
+        link: "https://github.com/Akis3479/Compilers-Project---hy340",
         more: "Engineered a full-fledged compiler leveraging Lex and Yacc-type tools, alongside the development of the alpha virtual machine and creation of essential library functions. ",
     },
     {
@@ -27,6 +30,7 @@ const cards = [
         id: "blue",
         description: "Technology : MEAN Stack, Docker",
         footer: "Three-Person University Project",
+        link: "https://github.com/Akis3479/hy469",
         more: "Crafted an Innovative Smart Room App tailored for collaborative brainstorming, featuring touch-sensitive walls for simultaneous input from all team members. Real-time feedback fosters an immersive creative process. Developed using MEAN stack seamlessly integrated with Docker for robust deployment and scalability. ",
     },
     {
@@ -34,6 +38,7 @@ const cards = [
         id: "green",
         description: "Technology : Java, mySQL, JDBC",
         footer: "Two-Person University Project",
+        link: "https://github.com/Akis3479/databaseProject-hy360",
         more: "Created a hospital application tailored for doctors and staff to efficiently manage COVID-19 emergency patients.  ",
     },
 ];
@@ -54,6 +59,8 @@ export const ProjectCard = ({ isOpen }) => {
                                 <var>{card.total}</var>
                                 <h3>{card.description}</h3>
                                 <h4>{card.footer}</h4>
+                                <a href={card.link} target="_blank"style={{display:"flex", alignItems:"center"}}>
+        <img src="github.svg" alt="Git logo" className="gitLink" style={{width:"10%", height:"10%"}}/></a>
                                 <div className="material-symbols-outlined"> more </div>
                             </div>
                             <div className="back">
